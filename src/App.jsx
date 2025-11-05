0f2dc4114738f4e617117e4324e19e5a48496375
momo-2005 <flower.momo20050119@gmail.com>
2025-11-05 10:34:39 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { InputForm } from "./components/InputForm";
+import Title from "./components/Title";
+import { TodoList } from "./components/TodoList";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="body">
+      <Title />
+      <InputForm taskList={taskList} setTaskList={setTaskList} />
+      <TodoList taskList={taskList} setTaskList={setTaskList} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div>
+        <header>
+            <h1>簡単なTodoリスト</h1>
+        </header>
+    </div>
+  )
+};
